b8da74907e89898567f18194fcb1bab81b4321fa
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-01-21 19:54:19 -0300

    Fix = Better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -246,7 +245,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted!");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) { // String é um tipo classe, não é um tipo primitivo
-			throw new InvalidParameterException("Invalid type for promotion!");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
